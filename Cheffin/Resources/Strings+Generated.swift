8bab54bc2513f983fa3fd3c6b761e2848a47d5b7
Martinus Andika Novanawa <69181762+AtlanticNova@users.noreply.github.com>
2023-06-27 16:58:44 +0700

    feat: Create RecipeCard Component (#8)
    * RecipeCard Component
    
    * Update RecipeCard.swift
    
    Change Image to AsyncImage so it can use image from the internet
    
    * Update RecipeCard.swift
    
    update Image on utensil & update tooltip configuration on utensil
    
    ---------
    
    Co-authored-by: Adryan Eka Vandra <adryanekavandra@gmail.com>

--- Cheffin/Resources/Strings+Generated.swift
+++ Cheffin/Resources/Strings+Generated.swift
@@ -10,43 +10,43 @@ import Foundation
 // swiftlint:disable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:disable nesting type_body_length type_name vertical_whitespace_opening_braces
 internal enum S {
-    /// Localizable.strings
-    ///   Cheffin
-    ///
-    ///   Created by Adryan Eka Vandra on 23/06/23.
-    internal static let appName = S.tr("Localizable", "appName", fallback: "Chefin")
-    /// ====================== BON-APETIT PAGE ======================
-    internal static let bonApetit = S.tr("Localizable", "bonApetit", fallback: "Bon Apetit!")
-    /// Congratulations on successfully making your %@!
-    internal static func congratulationMessage(_ p1: Any) -> String {
-        return S.tr("Localizable", "congratulationMessage", String(describing: p1), fallback: "Congratulations on successfully making your %@!")
-    }
-    /// Essential Utensils
-    internal static let essentialUtensils = S.tr("Localizable", "essentialUtensils", fallback: "Essential Utensils")
-    /// ====================== RECIPE PAGE ======================
-    internal static let ingredients = S.tr("Localizable", "ingredients", fallback: "Ingredients")
-    /// Step-By-Step
-    internal static let stepByStep = S.tr("Localizable", "stepByStep", fallback: "Step-By-Step")
-    /// Step-By-Step Mode
-    internal static let stepByStepMode = S.tr("Localizable", "stepByStepMode", fallback: "Step-By-Step Mode")
-    /// ====================== STEP-BY-STEP PAGE ======================
-    internal static func stepOf(_ p1: Int, _ p2: Int) -> String {
-        return S.tr("Localizable", "stepOf", p1, p2, fallback: "%d of %d")
-    }
-    /// Take a picture
-    internal static let takeAPicture = S.tr("Localizable", "takeAPicture", fallback: "Take a picture")
-    /// try saying
-    internal static let trySaying = S.tr("Localizable", "trySaying", fallback: "try saying")
-    /// Utensils
-    internal static let utensils = S.tr("Localizable", "utensils", fallback: "Utensils")
-    /// "%@"
-    internal static func voiceCommand(_ p1: Any) -> String {
-        return S.tr("Localizable", "voiceCommand", String(describing: p1), fallback: "\"%@\"")
-    }
-    /// ====================== HOME PAGE ======================
-    internal static let welcomeToChefin = S.tr("Localizable", "welcomeToChefin", fallback: "Welcome to Chefin!")
-    /// What do you want to cook today?
-    internal static let whatDoYouWantToCookToday = S.tr("Localizable", "whatDoYouWantToCookToday?", fallback: "What do you want to cook today?")
+  /// Localizable.strings
+  ///   Cheffin
+  /// 
+  ///   Created by Adryan Eka Vandra on 23/06/23.
+  internal static let appName = S.tr("Localizable", "appName", fallback: "Chefin")
+  /// ====================== BON-APETIT PAGE ======================
+  internal static let bonApetit = S.tr("Localizable", "bonApetit", fallback: "Bon Apetit!")
+  /// Congratulations on successfully making your %@!
+  internal static func congratulationMessage(_ p1: Any) -> String {
+    return S.tr("Localizable", "congratulationMessage", String(describing: p1), fallback: "Congratulations on successfully making your %@!")
+  }
+  /// Essential Utensils
+  internal static let essentialUtensils = S.tr("Localizable", "essentialUtensils", fallback: "Essential Utensils")
+  /// ====================== RECIPE PAGE ======================
+  internal static let ingredients = S.tr("Localizable", "ingredients", fallback: "Ingredients")
+  /// Step-By-Step
+  internal static let stepByStep = S.tr("Localizable", "stepByStep", fallback: "Step-By-Step")
+  /// Step-By-Step Mode
+  internal static let stepByStepMode = S.tr("Localizable", "stepByStepMode", fallback: "Step-By-Step Mode")
+  /// ====================== STEP-BY-STEP PAGE ======================
+  internal static func stepOf(_ p1: Int, _ p2: Int) -> String {
+    return S.tr("Localizable", "stepOf", p1, p2, fallback: "%d of %d")
+  }
+  /// Take a picture
+  internal static let takeAPicture = S.tr("Localizable", "takeAPicture", fallback: "Take a picture")
+  /// try saying
+  internal static let trySaying = S.tr("Localizable", "trySaying", fallback: "try saying")
+  /// Utensils
+  internal static let utensils = S.tr("Localizable", "utensils", fallback: "Utensils")
+  /// "%@"
+  internal static func voiceCommand(_ p1: Any) -> String {
+    return S.tr("Localizable", "voiceCommand", String(describing: p1), fallback: "\"%@\"")
+  }
+  /// ====================== HOME PAGE ======================
+  internal static let welcomeToChefin = S.tr("Localizable", "welcomeToChefin", fallback: "Welcome to Chefin!")
+  /// What do you want to cook today?
+  internal static let whatDoYouWantToCookToday = S.tr("Localizable", "whatDoYouWantToCookToday?", fallback: "What do you want to cook today?")
 }
 // swiftlint:enable explicit_type_interface function_parameter_count identifier_name line_length
 // swiftlint:enable nesting type_body_length type_name vertical_whitespace_opening_braces
@@ -54,20 +54,20 @@ internal enum S {
 // MARK: - Implementation Details
 
 extension S {
-    private static func tr(_ table: String, _ key: String, _ args: CVarArg..., fallback value: String) -> String {
-        let format = BundleToken.bundle.localizedString(forKey: key, value: value, table: table)
-        return String(format: format, locale: Locale.current, arguments: args)
-    }
+  private static func tr(_ table: String, _ key: String, _ args: CVarArg..., fallback value: String) -> String {
+    let format = BundleToken.bundle.localizedString(forKey: key, value: value, table: table)
+    return String(format: format, locale: Locale.current, arguments: args)
+  }
 }
 
 // swiftlint:disable convenience_type
 private final class BundleToken {
-    static let bundle: Bundle = {
-        #if SWIFT_PACKAGE
-        return Bundle.module
-        #else
-        return Bundle(for: BundleToken.self)
-        #endif
-    }()
+  static let bundle: Bundle = {
+    #if SWIFT_PACKAGE
+    return Bundle.module
+    #else
+    return Bundle(for: BundleToken.self)
+    #endif
+  }()
 }
 // swiftlint:enable convenience_type
