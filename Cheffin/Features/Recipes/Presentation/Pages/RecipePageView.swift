f4324a2d2a1107ca7b920634468b46e5d2397ce8
Adryan Eka Vandra <adryanekavandra@gmail.com>
2023-07-01 22:07:23 +0700

    fix: merapikan ui home dan recipe (#25)

--- Cheffin/Features/Recipes/Presentation/Pages/RecipePageView.swift
+++ Cheffin/Features/Recipes/Presentation/Pages/RecipePageView.swift
@@ -12,82 +12,129 @@ struct RecipePageView: View {
 	 var recipe: Recipe
 	@State var isStepByStepModeOn = false
 	@StateObject var viewModel: RecipeDetailViewModel
+	@Environment(\.dismiss)
+	private var dismiss
     
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading) {
-                VStack {
-					AsyncImage(url: URL(string: recipe.image)) { image in
-						image
-							.resizable()
-							.aspectRatio(contentMode: .fill)
-					} placeholder: {
-						ProgressView()
+		ScrollView {
+			ZStack {
+				VStack(alignment: .leading) {
+					Group {
+						VStack {
+							AsyncImage(url: URL(string: recipe.image)) { image in
+								image
+									.resizable()
+									.scaledToFill()
+									.frame(
+										width: UIScreen.main.bounds.width,
+										height: UIScreen.main.bounds.height * (1 / 3))
+									.clipped()
+							} placeholder: {
+								ProgressView()
+									.padding(.top, 48)
+							}
+							.contentShape(Rectangle())
+						}
 					}
-                }
-                HStack {
-					Text(recipe.name)
-                        .padding(.horizontal)
-                        .font(Font.title.weight(.bold))
-                }
-                HStack {
-                    Image("clock")
-                        .padding(.leading)
-					Text(recipe.duration)
-                }
-                .padding(.bottom)
-                HStack {
-					Text(recipe.description)
-                        .padding(.horizontal)
-                        .multilineTextAlignment(.leading)
-                        .font(.body)
-                }
-                .padding(.bottom)
-                HStack {
-					Text(S.ingredients)
-                        .font(Font.title2.weight(.bold))
-                        .padding(.leading)
-                }
-				IngredientsGridView(ingredients: recipe.ingredients)
-                    .padding(.bottom)
-                
-                HStack {
-					Text(S.utensils)
-                        .font(Font.title2.weight(.bold))
-                        .padding(.leading)
-                    Spacer()
-                }
-				UtensilsGridView(utensils: recipe.utensils)
-                    .padding(.bottom)
-                
-                HStack {
-					Text(S.stepByStep)
-                        .font(Font.title2.weight(.bold))
-                        .padding(.leading)
-                    Spacer()
-                }
-                .padding(.bottom)
+					Group {
+						HStack {
+							Text(recipe.name)
+								.padding(.horizontal)
+								.font(Font.title.weight(.bold))
+						}
+						HStack {
+							Image("clock")
+								.padding(.leading)
+							Text(recipe.duration)
+						}
+						.padding(.bottom)
+					}
 				
-				StepsView(instructions: recipe.instructions)
-                    .padding(.bottom)
-            }
-            
-            Button(action: {
-				isStepByStepModeOn.toggle()
-                        // Button action code
-                    }) {
-                        Text("Let's Cook!")
-                            .font(.headline)
-                            .frame(width: 358, height: 44)
-                            .background(Color(red: 246 / 255, green: 200 / 255, blue: 79 / 255))
-                            .cornerRadius(10)
-                            .foregroundColor(.black)
-                    }
-            
+					Group {
+						HStack {
+							Text(recipe.description)
+								.padding(.horizontal)
+								.multilineTextAlignment(.leading)
+								.font(.body)
+						}
+						.padding(.bottom)
+					}
+					
+					Group {
+						HStack {
+							Text(S.ingredients)
+								.font(Font.title2.weight(.bold))
+								.padding(.leading)
+						}
+						IngredientsGridView(ingredients: recipe.ingredients)
+							.padding(.bottom)
+						
+						HStack {
+							Text(S.utensils)
+								.font(Font.title2.weight(.bold))
+								.padding(.leading)
+							Spacer()
+						}
+						UtensilsGridView(utensils: recipe.utensils)
+							.padding(.bottom)
+					}
+					
+					Group {
+						HStack {
+							Text(S.stepByStep)
+								.font(Font.title2.weight(.bold))
+								.padding(.leading)
+							Spacer()
+						}
+						.padding(.bottom)
+						
+						StepsView(instructions: recipe.instructions)
+							.padding(.bottom)
+						
+					}
+					Button {
+						isStepByStepModeOn.toggle()
+					} label: {
+						Text("Let's Cook!")
+							.font(.headline)
+							.frame(
+								maxWidth: .infinity
+
+							)
+							.cornerRadius(10)
+							.foregroundColor(.black)
+					}
+					.buttonStyle(.borderedProminent)
+					.tint(I.primary.swiftUIColor)
+
+					.padding()
+
+
+
+				}
+				
+				VStack {
+					HStack {
+						Image(systemName: "chevron.backward")
+							.resizable()
+							.foregroundColor(.white)
+							.frame(width: 16, height: 24)
+							.clipped()
+							.onTapGesture {
+								dismiss()
+							}
+						Spacer()
+					}
+					Spacer()
+				}
+				.padding(EdgeInsets(top: 48, leading: 24, bottom: 48, trailing: 24))
+			}
+			.sheet(isPresented: $isStepByStepModeOn) {
+				StepByStepPageView(viewModel.state.stepBySteps)
+				
+			}
 		}
-		.sheet(isPresented: $isStepByStepModeOn) {
-			StepByStepPageView(viewModel.state.stepBySteps)
-		}
+		.ignoresSafeArea(edges: .top)
     }
 }
 
